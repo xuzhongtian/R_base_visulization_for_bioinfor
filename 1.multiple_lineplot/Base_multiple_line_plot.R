e7e872bba968b3313b52bb51a70b37d008a12198
xuzhongtian11@163.com <xuzhongtian11@163.com>
2018-12-30 05:18:55 +0800

    upload multiple line plot

---
+++ 1.multiple_lineplot/Base_multiple_line_plot.R
@@ -0,0 +1,29 @@
+rm(list=ls())
+
+library(RColorBrewer)
+
+
+colours  = brewer.pal(11, "Set3")[c(1,4,5,7)]
+xy <- par("usr")
+par(las=2,lwd=1)
+par(mar=c(5,4,4,2)+c(2,2,2,2),mfrow=c(1,1))
+
+data <- read.csv("./Zscore_cluster.csv", header = TRUE, row.names = 1)
+for (i in sort(unique(data$cluster))){
+  tmp = subset(data, cluster == i)[-5]
+  avg = apply(tmp, 2, mean)
+  
+  pdf(paste0("cluster_",i,"_lineplot.pdf"), width = 10)
+  plot(avg, type = "l", ylim = c(min(tmp)*1.1, max(tmp)*1.1),ylab = "Z-Score value",
+       xaxt="n", xlab = "", col=colours[2], lwd=2 )
+  title(main = paste0("Expression_pattern","_cluster_",i), cex.main = 1.2, font.main=1.5, line=1.5)
+  text(c(1,2,3,4),par("usr")[3] * 1.1, labels = colnames(data)[-5], xpd=TRUE, cex=1.2)
+  
+  drawline <- function(x){
+    lines(c(1,2,3,4),x,lwd=1.1, col=colours[1])
+  }
+  apply(tmp, 1, drawline)
+  lines(c(1,2,3,4), avg, lwd=2, col=colours[2])
+  dev.off()
+}
+
--- README.md
+++ README.md
@@ -6,5 +6,5 @@ This reposity mainly record how to visulize the NGS omics data via R.
 
 + **Background**: To study the expression pattern of differentially expression genes in NGS datasets, expecially in transcriptome datasets, we often cluster the DEG expression matrix to several groups according to their expression pattern(Such as K-means clustering). In order to gain a explicit understanding of those groups, we sometimes choose to visualize them with multiple-line plot. 
 + **input**: expression matrix with last column tagged with cluster ID 
-+ **how to use it**: Rscript multiple_line_plot.R
++ **how to use it**: Rscript Base_multiple_line_plot.R
 + **output**: several figures pointed to different expression pattern group. 
