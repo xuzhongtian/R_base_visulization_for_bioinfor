9533f24775e9ebfe93c54be3e59928497fb880aa
xuzhongtian11@163.com <xuzhongtian11@163.com>
2018-12-30 04:42:43 +0800

    multiple line plot

--- 1.multiple_lineplot/multiple_line_plot.R
+++ 1.multiple_lineplot/multiple_line_plot.R
@@ -8,15 +8,15 @@ xy <- par("usr")
 par(las=2,lwd=1)
 par(mar=c(5,4,4,2)+c(2,2,2,2),mfrow=c(1,1))
 
-data <- read.csv("./0135km9.csv", header = TRUE, row.names = 1)
+data <- read.csv("./Zscore_cluster.csv", header = TRUE, row.names = 1)
 for (i in sort(unique(data$cluster))){
   tmp = subset(data, cluster == i)[-5]
   avg = apply(tmp, 2, mean)
   
   pdf(paste0("cluster_",i,"_lineplot.pdf"), width = 10)
-  plot(avg, type = "l", ylim = c(min(tmp)*1.1, max(tmp)*1.1),ylab = "blabla xiao mo xian",
+  plot(avg, type = "l", ylim = c(min(tmp)*1.1, max(tmp)*1.1),ylab = "Z-Score value",
        xaxt="n", xlab = "", col=colours[2], lwd=2 )
-  title(main = paste0("Quan shijie wo zui ke ai","_cluster_",i), cex.main = 1.2, font.main=1.5, line=1.5)
+  title(main = paste0("Expression_pattern_","_cluster_",i), cex.main = 1.2, font.main=1.5, line=1.5)
   text(c(1,2,3,4),par("usr")[3] * 1.1, labels = colnames(data)[-5], xpd=TRUE, cex=1.2)
   
   drawline <- function(x){
